084de030a2348e98af88319553d26873c10531cd
Daniel Reintanz <85614916+FreelancerAMP@users.noreply.github.com>
2024-04-02 14:54:38 +0200

    UPDATED setup logic ADDED new shortcode and polylang support

--- gpen-setup.php
+++ gpen-setup.php
@@ -3,19 +3,19 @@
 class Gpen_Setup {
 
 	function __construct() {
-		add_action( 'wp_enqueue_scripts', function() {
+		add_action( 'wp_enqueue_scripts', function () {
 			if ( is_page_template( 'page-list.php' ) ) {
 				wp_enqueue_style( 'leaflet', 'https://unpkg.com/leaflet@1.5.1/dist/leaflet.css', [], '1.5.1' );
 				wp_enqueue_script( 'leaflet', 'https://unpkg.com/leaflet@1.5.1/dist/leaflet.js', [], '1.5.1', true );
 
-				wp_enqueue_script( 'gpen-map', plugin_dir_url(__FILE__) , ['jquery', 'leaflet'], '1.0', true );
+				wp_enqueue_script( 'gpen-map', plugin_dir_url( __FILE__ ), [ 'jquery', 'leaflet' ], '1.0', true );
 
 				$locations = [];
-				$query = new WP_Query( [
-					'post_type' => 'restaurant',
+				$query     = new WP_Query( [
+					'post_type' => 'location',
 				] );
 				foreach ( $query->posts as $post ) {
-					$location            = rwmb_get_value( 'location', '', $post->ID );
+					$location            = get_post_meta( $post->ID,'osm_location',true );
 					$location['title']   = $post->post_title;
 					$location['address'] = rwmb_get_value( 'address', '', $post->ID );
 					$location['icon']    = rwmb_get_value( 'icon', '', $post->ID );
@@ -24,8 +24,17 @@ class Gpen_Setup {
 				wp_localize_script( 'gpen-map', 'Locations', $locations );
 			}
 		} );
+		add_shortcode( 'display_gpen_map', array( $this, 'display_gpen_map' ) );
 	}
 
+
+	function display_gpen_map() {
+		ob_start();
+		?>
+        <div id="map" style="width: 100%; height: 600px"></div>
+		<?php
+		return ob_get_clean();
+	}
 
 
 }
--- gpenref-relaunch.php
+++ gpenref-relaunch.php
@@ -12,14 +12,50 @@
  * GitHub Branch:     master
  */
 
-class Gpenref_Relauch{
+require_once( "gpen-setup.php" );
 
 
-    function __construct()
-    {
+class Gpenref_Relauch {
 
-    }
+
+	function __construct() {
+		add_action( 'init', array( $this, 'add_plugin_pll_strings' ) );
+		add_shortcode( 'gpen_frontpage_map', array( $this, 'display_frontpage_map' ) );
+
+	}
+
+	function add_plugin_pll_strings() {
+		if ( function_exists( 'pll_register_string' ) ) {
+			pll_register_string( 'gpen_participants', 'Participants Worldwide' );
+		}
+
+	}
+
+	function display_frontpage_map() {
+		$places            = get_posts( array( 'posttype' => 'places', 'numberposts' => - 1 ) );
+		$number_places     = count( $places );
+		$places_string_pll = 'Participants Worldwide';
+		if ( function_exists( 'pll__' ) ) {
+			$places_string_pll = ppl__( 'gpen_participants' );
+		}
+		ob_start();
+
+		?>
+
+        <div class="gpen-frontpage-map">
+            <img src="<?php echo plugin_dir_url( __FILE__ ) . 'assets/gpen_illustration_startseite_2.png' ?>">
+            <div class="gpen-banner-places">
+                <div><?php echo $number_places ?></div>
+                <div><?php echo $places_string_pll ?></div>
+            </div>
+        </div>
+		<?php
+
+		return ob_get_clean();
+
+	}
 
 }
+
 new Gpenref_Relauch();
 new Gpen_Setup();
